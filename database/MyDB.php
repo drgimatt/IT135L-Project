e1fec0d96f9f842362c06a6e1b511cc2c5289429
Miguel Escandor <boxrox12@gmail.com>
2024-04-18 15:14:55 +0800

    added userrole db

--- database/MyDB.php
+++ database/MyDB.php
@@ -8,6 +8,18 @@
    if(!$db) {
       echo $db->lastErrorMsg();
    } else {
-      echo "Opened database successfully\n";
+      echo "Opened database successfully\n\n";
+      
+      // Create userrole table
+      $query = "CREATE TABLE IF NOT EXISTS userrole (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    rolename VARCHAR NOT NULL
+                )";
+      $result = $db->exec($query);
+      if (!$result) {
+          echo "Error creating userrole table: " . $db->lastErrorMsg();
+      } else {
+          echo "Userrole table created successfully\n";
+      }
    }
 ?>
