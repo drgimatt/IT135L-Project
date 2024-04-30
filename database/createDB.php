3a2490309b252c1bff0065533dd88b5d2ab745e8
Miguel Escandor <boxrox12@gmail.com>
2024-05-01 07:40:37 +0800

    copied generate table statements from previous commits

--- database/createDB.php
+++ database/createDB.php
@@ -43,4 +43,65 @@
         echo "Error creating table: " . $e->getMessage();
     }
 
+    $userroleTableQuery = "CREATE TABLE userrole (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        rolename VARCHAR NOT NULL
+    )";
+
+    $articlecategoryTableQuery = "CREATE TABLE article_category (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        category VARCHAR NOT NULL
+    )";
+
+    $itemcategoryTableQuery = "CREATE TABLE item_category (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        category VARCHAR NOT NULL
+    )";
+
+    $employeeTableQuery = "CREATE TABLE employee (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        firstname VARCHAR NOT NULL,
+        middlename VARCHAR,
+        lastname VARCHAR NOT NULL,
+        position VARCHAR NOT NULL,
+        email VARCHAR NOT NULL,
+        gender VARCHAR NOT NULL,
+        contact VARCHAR
+    )";
+
+    $inventoryTableQuery = "CREATE TABLE inventory (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        image LONGBLOB,
+        name VARCHAR NOT NULL,
+        category_id INTEGER,
+        value INTEGER NOT NULL,
+        quantity INTEGER NOT NULL,
+        dateadded VARCHAR NOT NULL,
+        datemodified VARCHAR NOT NULL,
+        FOREIGN KEY(category_id) REFERENCES item_category(id)
+    )";
+
+    $articlesTableQuery = "CREATE TABLE articles (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        employee_id INTEGER,
+        title VARCHAR NOT NULL,
+        category_id INTEGER,
+        author VARCHAR NOT NULL,
+        description VARCHAR NOT NULL,
+        FOREIGN KEY(category_id) REFERENCES article_category(id)
+        FOREIGN KEY(employee_id) REFERENCES employee(id)
+    )";                        
+
+    $credentialsTableQuery = "CREATE TABLE credentials (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        employee_id INTEGER,
+        username VARCHAR,
+        password VARCHAR,
+        datecreated VARCHAR,
+        role_id INTEGER,
+        FOREIGN KEY(role_id) REFERENCES userrole(id),
+        FOREIGN KEY(employee_id) REFERENCES employee(id)
+    )";
+
+
 ?>
